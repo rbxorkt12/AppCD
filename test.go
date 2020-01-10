9991a3660a052661139b66aad190e1ad2433d804
Gyutae Park <gtp7473@sk.com>
2020-01-10 10:53:54 +0900

    test

--- test.go
+++ test.go
@@ -3,11 +3,25 @@ package main
 import (
 	"fmt"
 	"github.com/rbxorkt12/applink/pkg/argocd"
+	"github.com/rbxorkt12/applink/pkg/config"
 )
 
 func main(){
+	config,err:=config.GetConfig("example")
+	if err!= nil {panic(err)}
 	argoinfo,err:=argocd.ArgocdSet()
-	if err!=nil {panic(err)}
-	fmt.Println(argoinfo)
+	if err!= nil {panic(err)}
+	configitem,err:=argocd.GetappsinCluster(*argoinfo)
+	if err!= nil {panic(err)}
+	clusteritem :=argocd.GetappsinConfig(config)
+	fmt.Println("this is clusteritem")
+	for _,item1:= range clusteritem{
+		fmt.Println(item1)
+	}
+	fmt.Println("this is configitem")
+	for _,item2:= range configitem{
+		fmt.Println(item2)
+	}
+
 
 }
